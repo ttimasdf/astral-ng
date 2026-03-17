41fd8b0798951bff76620eb85665bb54a7966eec
ttimasdf <opensource@rabit.pw>
2026-03-17 21:58:19 +0800

    chore: remove hardcoded servers from blocklist
    Remove specific hardcoded entries (629957.xyz) from the blocked URLs list. This cleans up the configuration by eliminating these individual rules.

--- lib/shared/utils/network/blocked_servers.dart
+++ lib/shared/utils/network/blocked_servers.dart
@@ -1,8 +1,5 @@
 class BlockedServers {
   static const List<String> blockedUrls = [
-    'js.629957.xyz:11012',
-    'nmg.629957.xyz:11010',
-    'nmg.629957.xyz:11010',
   ];
 
   static bool isBlocked(String url) {
